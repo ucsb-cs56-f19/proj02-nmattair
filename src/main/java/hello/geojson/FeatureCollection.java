6d91965268595a861f632358a3d49b53aaa68474
Nick Mattair <nmattair@ucsb.edu>
2019-11-15 14:06:29 -0800

    nm - added metadata

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 public class FeatureCollection {
     private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
     
+    public Metadata metadata;
     public String type;
 
      /**
