2aa164e03895b6e689c955886d0a84e95d9477c5
Nick Mattair <nmattair@ucsb.edu>
2019-11-14 18:55:04 -0800

    nm - created form

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,25 @@
+package hello;
+
+public class EqSearch {
+    private int distance;
+    private int minmag;
+
+    public EqSearch(){};
+
+    public void setDistance(int dist){
+        distance= dist;
+    }
+
+    public void setMinmag(int mm){
+        minmag= mm;
+    }
+
+    public int getDistance(){
+        return distance;
+    }
+
+    public int getMinmag(){
+        return minmag;
+    }
+
+}
