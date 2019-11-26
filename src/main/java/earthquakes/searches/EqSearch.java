c43146d55acae37245c6c009bcb5b136c11ab8b5
Nick Mattair <nmattair@ucsb.edu>
2019-11-26 13:38:00 -0800

    nm - added fields for lat, lon, and location

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -3,6 +3,9 @@ package earthquakes.searches;
 public class EqSearch {
     private int distance;
     private int minmag;
+    private int lat;
+    private int lon;
+    private String location;
 
     public EqSearch(){};
 
@@ -14,12 +17,36 @@ public class EqSearch {
         minmag= mm;
     }
 
+    public void setLat(int la){
+        lat= la;
+    }
+
+    public void setLon(int lo){
+        lon= lo;
+    }
+
+    public void setLocation(String loc){
+        location= loc;
+    }
+
     public int getDistance(){
         return distance;
     }
 
     public int getMinmag(){
         return minmag;
+    }
+
+    public int getLat(){
+        return lat;
+    }
+
+    public int getLon(){
+        return lon;
+    }
+
+    public String getLocation(){
+        return location;
     }
 
 }
